9f39df07c3d9ba2549d0f495095930ae4c635702
afesuazo <afesuazo@gmail.com>
2023-08-31 18:59:52 -0500

    Event listener documentation

--- include/event_listener.h
+++ include/event_listener.h
@@ -8,17 +8,49 @@
 
 namespace event_manager {
 
+    /**
+     * @class EventListenerBase
+     *
+     * @brief Interface for all event listeners, intended for polymorphism.
+     *
+     * Provides a base interface for all event listeners. This allows us to easily create containers for
+     * EventListeners and handle multiple types of events in a uniform manner.
+     *
+     * @note Should not be inherited from directly, instead use EventListener
+     *
+     */
     class EventListenerBase {
     public:
         virtual ~EventListenerBase() = default;
+
+        /**
+         * @brief Called when an event this listener subscribed to is triggered
+         *
+         * @param event Reference to the event that was triggered.
+         */
         virtual void OnEvent(IEvent& event) = 0;
     };
 
+    /**
+     * @class EventListener
+     * @brief Templated interface for creating a listener of a specific event type
+     *
+     * @tparam TEvent The event type this listener is intended to subscribe to
+     * @note The `TEvent` type must be derived from `IEvent`
+     */
     template<typename TEvent>
     class EventListener : public EventListenerBase {
         static_assert(std::is_base_of<IEvent, TEvent>::value, "TEvent must be be derived from IEvent");
     public:
 
+        /**
+         * @brief Overridden method to handle type-erasure. Will immediately call the expected OnEvent method.
+         *
+         * Dynamically casts the event reference to the expected event type (TEvent). If types match,
+         * an virtual OnEvent function with the same signature gets called.
+         *
+         * @param event The event that was triggered.
+         */
         void OnEvent(IEvent& event) override {
             if (typeid(event) == typeid(TEvent)) {
                 OnEvent(dynamic_cast<TEvent&>(event));
@@ -26,9 +58,9 @@ namespace event_manager {
         }
 
         /**
-         * @brief Called when an event this listener is subscribed to gets triggered.
+         * @brief Called when an event this listener subscribed to is triggered
          *
-         * @param eventName Name of the event that was triggered.
+         * @param event Reference to the event (of type TEvent) that was triggered.
          */
         virtual void OnEvent(TEvent& event) = 0;
     };
