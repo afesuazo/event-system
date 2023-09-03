bf3988299c413c9c2e5371d4d832fbb1fb14ad5b
afesuazo <afesuazo@gmail.com>
2023-09-03 12:55:38 -0500

    Base event emitter

---
+++ include/event_emitter.h
@@ -0,0 +1,40 @@
+//
+// Created by Andres Suazo
+//
+
+#pragma once
+#include <iostream>
+#include "event_manager.h"
+#include "base_event.h"
+
+namespace event_manager {
+
+    class EventEmitter {
+    public:
+
+        EventEmitter();
+        ~EventEmitter();
+
+        template <typename TEvent>
+        void Emit(TEvent& event) {
+            if (!IsAllowedEvent(event)) {
+                std::cerr << "Event type not allowed.\n";
+                return;
+            }
+
+            if (auto sharedManager = eventManager.lock()) {
+                sharedManager->EmitEvent(event);
+            }
+        }
+
+    protected:
+        virtual bool IsAllowedEvent(const BaseEvent& event) {
+            // By default, all event types are allowed
+            return true;
+        }
+
+    private:
+        std::weak_ptr<EventManager> eventManager;
+    };
+
+}
