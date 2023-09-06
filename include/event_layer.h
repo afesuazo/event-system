9fadf1cd428d17e3691f6abb8832b4d1dc269939
afesuazo <afesuazo@gmail.com>
2023-09-05 20:30:09 -0500

    Base layer encapsulation changes

--- include/event_layer.h
+++ include/event_layer.h
@@ -12,39 +12,49 @@ namespace event_manager {
 
     class EventLayer {
     public:
-        explicit EventLayer(std::shared_ptr<EventManager>& eventManager) : eventManager(eventManager) {}
+        explicit EventLayer() : eventEmitter(eventManager) {
+            eventManager = std::make_shared<EventManager>();
+        }
 
         virtual ~EventLayer() = default;
 
+        virtual void Run() = 0;
+
+        // For events sourced from external layers
+        void OnEvent(BaseEvent& event) {
+            eventEmitter.Emit(event);
+        };
+
+
+    protected:
+
         template<typename TEvent>
         void AddListener(const std::shared_ptr<IEventListener<TEvent>>& listener) {
-            if (auto sharedManager = eventManager.lock()) {
-                // Register listener
-                sharedManager->AddSubscriber(listener);
-            }
+            eventManager->AddSubscriber(listener);
         }
 
         template<typename TEvent>
         void RemoveListener(const std::shared_ptr<IEventListener<TEvent>>& listener) {
-            if (auto sharedManager = eventManager.lock()) {
-                // Register listener
-                sharedManager->RemoveSubscriber(listener);
-            }
+            eventManager->RemoveSubscriber(listener);
         }
 
-        // Triggers an event on the local event manager
-        void EmitEvent(BaseEvent& event) {
-            if (auto sharedManager = eventManager.lock()) {
-                // Register listener
-                sharedManager->RemoveSubscriber(listener);
-            }
+        // TODO: Default to all or none?
+        void SetAllowedEvents() {
+
         };
 
-    protected:
-        // Points to the shared event manager
-        std::weak_ptr<EventManager> eventManager;
-        std::weak_ptr<EventEmitter> eventEmitter;
+        // Even triggered within the layer
+        void TriggerEvent(BaseEvent& event) {
+            eventEmitter.Emit(event);
+        };
+
         std::string name;
+
+    private:
+        // Points to the shared event manager
+        std::shared_ptr<EventManager> eventManager;
+        // Used to emit local and global events
+        EventEmitter eventEmitter;
     };
 
 }
