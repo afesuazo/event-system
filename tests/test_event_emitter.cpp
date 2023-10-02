763c756ff26342d5215514bb764c2d261738a859
afesuazo <afesuazo@gmail.com>
2023-10-02 18:05:37 -0500

    Update emitter tests

--- tests/test_event_emitter.cpp
+++ tests/test_event_emitter.cpp
@@ -28,7 +28,7 @@ protected:
 
     void SetUp() override {
         event_manager = std::make_shared<EventManager>();
-        event_emitter = EventEmitter{event_manager};
+        event_emitter = EventEmitter{event_manager, nullptr};
     }
 };
 
@@ -48,10 +48,31 @@ TEST_F(EventEmitterTest, InvalidEventEmissionTest) {
     event_manager->AddSubscriber(general_event_handler);
 
     GeneralEvent general_event{GeneralEvents::GeneralSubType0};
-    SpecificOnlyEventEmitter specific_event_emitter{event_manager};
+    SpecificOnlyEventEmitter specific_event_emitter{event_manager, nullptr};
 
     specific_event_emitter.Emit(general_event);
 
     auto casted_handler = std::static_pointer_cast<TestEventHandler<GeneralEvent>>(general_event_handler);
     EXPECT_FALSE(casted_handler->event_triggered);
+}
+
+TEST_F(EventEmitterTest, NullManagerAndHandlerTest) {
+    EventEmitter null_emitter;
+    GeneralEvent general_event{GeneralEvents::GeneralSubType0};
+
+    EXPECT_NO_THROW(null_emitter.Emit(general_event));
+}
+
+TEST_F(EventEmitterTest, ExternalManagerPropagationTest) {
+    std::shared_ptr<EventManager> external_manager = std::make_shared<EventManager>();
+    std::shared_ptr<IEventHandler<GeneralEvent>> external_handler = std::make_shared<TestEventHandler<GeneralEvent>>();
+    external_manager->AddSubscriber(external_handler);
+
+    EventEmitter emitter{event_manager, external_manager};
+
+    GeneralEvent general_event{GeneralEvents::GeneralSubType0};
+    emitter.Emit(general_event);
+
+    auto casted_handler = std::static_pointer_cast<TestEventHandler<GeneralEvent>>(external_handler);
+    EXPECT_TRUE(casted_handler->event_triggered);
 }
