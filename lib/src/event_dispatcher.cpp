1582c957a6e1d345b1a0783196e719aeaa14fa14
afesuazo <afesuazo@gmail.com>
2024-01-29 19:14:05 -0600

    Add back lib files

---
+++ lib/src/event_dispatcher.cpp
@@ -0,0 +1,10 @@
+
+#include "event_dispatcher.h"
+
+namespace event_system {
+
+    [[nodiscard]] std::size_t EventDispatcher::GetHandlerCount() const {
+        return handlers_.size();
+    }
+
+}
